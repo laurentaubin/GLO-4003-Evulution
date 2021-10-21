c7d8f429303bc4a16969a6182efb519a6bb9b007
laurentaubin <44675437+laurentaubin@users.noreply.github.com>
2021-10-21 14:22:39 -0400

    [Fix] Catch exceptions in task executor (#144)

--- src/test/java/ca/ulaval/glo4003/ws/infrastructure/schedule/WeeklyTaskExecutorTest.java
+++ src/test/java/ca/ulaval/glo4003/ws/infrastructure/schedule/WeeklyTaskExecutorTest.java
@@ -1,14 +1,17 @@
 package ca.ulaval.glo4003.ws.infrastructure.schedule;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import ca.ulaval.glo4003.ws.domain.assembly.AssemblyLine;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class WeeklyTaskExecutorTest {
@@ -28,5 +31,17 @@ class WeeklyTaskExecutorTest {
     weeklyTaskExecutor.run();
 
     verify(assemblyLine, times(1)).advance();
+  }
+
+  @Test
+  public void givenAdvanceThrowsException_whenRun_thenDoNotThrowExceptionAndKeepGoing() {
+    // given
+    doThrow(new RuntimeException()).when(assemblyLine).advance();
+
+    // when
+    Executable running = () -> weeklyTaskExecutor.run();
+
+    // then
+    assertDoesNotThrow(running);
   }
 }
